e19b91e0c81b9cdb118a3d68409354230189d25b
rakhi-who <142892022+rakhi-who@users.noreply.github.com>
2025-08-05 15:49:22 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,96 @@
+let data = [
+  {
+    quest: "what is your discord username?",
+    answer: {
+      1: "jerry",
+      2: ">.&lt;jerry",
+      3: "jerry>&lt;",
+      4: ">•&lt;jerry",
+    },
+    correct: 4,
+  },
+  {
+    quest: "what does bit mean?",
+    answer: {
+      1: "bitch",
+      2: "little",
+      3: "bitcoin",
+      4: "silly",
+    },
+    correct: 1,
+  },
+  {
+    quest: "what is Ayush's birthday?",
+    answer: {
+      1: "28 march",
+      2: "28 january",
+      3: "30 may",
+      4: "idk",
+    },
+    correct: 1,
+  },
+  {
+    quest: "who  is sinister_perhaps?",
+    answer: {
+      1: "discord friend",
+      2: "instagram friend",
+      3: "someone i know",
+      4: "idk",
+    },
+    correct: 1,
+  },
+  {
+    quest: "what does eye emoji means?",
+    answer: {
+      1: "yo,wyd",
+      2: "i see you",
+      3: "idk",
+      4: "idc",
+    },
+    correct: 1,
+  },
+  {
+    quest: "what is magic number?",
+    answer: {
+      1: "89663652656",
+      2: "88348799018",
+      3: "3377377237",
+      4: "77e887we9wq7e",
+    },
+    correct: 4,
+  },
+];
+export const texts =  [
+  {
+    gifs: "./imgs/hehe.mp4",
+    texts: "pls pls pls , can you please forgive  ?",
+    response2: "idk",
+    response1: "i'll think of that",
+},
+  {
+    gifs: "./imgs/uwu.mp4",
+    texts: "A thousand apologies 🥹🌺 ?",
+    response2: "no",
+    response1: "still thinking",
+},
+  {
+    gifs: "./imgs/pls.mp4",
+    texts: " i did all for you to forgive me TT ?",
+    response2: "its literally wake up gif",
+    response1: "umm...",
+},
+  {
+    gifs: "./imgs/please.mp4",
+    texts: " are you still angry?",
+    response2: "yes , ofc!!",
+    response1: "may be or not",
+},
+  {
+    gifs: "./imgs/TT.mp4",
+    texts: " so ... 😭 ?",
+    response2: "never",
+    response1: "okie!!!🙂‍↕️🙂‍↕️",
+},
+   
+]
+export default data;
